cda48740205971ad76d98672fb0ba7e5bec45865
erika <erika.lasprilla@aerin.es>
2025-03-17 19:58:40 +0100

    Añado etiquetas de contenedores <div> para mejorar la maquetación responsive de la página.

--- prueba-css/src/index.js
+++ prueba-css/src/index.js
@@ -1,26 +1,35 @@
-
 var arrayTest = [1, 2, 3, 3, 2, 1, 4];
 
 document.getElementById("app").innerHTML = `
   <h1>Unique Element Challenge</h1>
+  <div class="grid-container">
+    <div>
+      <h2>Target</h2>
+      <div>Find the unique value in the array that is not duplicated. Use the <b>dubbed</b> function to return the unique element.</div>
 
-  <h2>Target</h2>
-  <div>Find the unique value in the array that is not duplicated. Use the <b>dubbed</b> function to return the unique element.</div>
-
-  <h2>Considerations</h2>
-  <ul>
-    <li>Values are duplicated only twice.</li>
-    <li>There is only one non duplicate value.</li>
-    <li>The non duplicate value can be placed anywhere on the array.</li>
-    <li>The result must be an integer.</li>
-  </ul>
-  
-  <h2>Test Data</h2>
-  <div>${arrayTest}</div>
-    
-  <h2>Expected result</h2>
-  <div>4</div>
-
-  <h2>Your Result</h2>
-  <div>4</div>
+      <h2>Considerations</h2>
+      <ul>
+        <li>Values are duplicated only twice.</li>
+        <li>There is only one non duplicate value.</li>
+        <li>The non duplicate value can be placed anywhere on the array.</li>
+        <li>The result must be an integer.</li>
+      </ul>
+    </div>
+    <div>
+      <div class="data">
+        <h2>Test Data</h2>
+        <div>${arrayTest}</div>
+      </div>
+      <div class="result">
+        <div>
+          <h2>Expected result</h2>
+          <div>4</div>
+        </div>
+        <div>
+          <h2>Your Result </h2>
+          <div>4</div>
+        </div>
+      </div>
+    </div>
+  </div>
 `;
