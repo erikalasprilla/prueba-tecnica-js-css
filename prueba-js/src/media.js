9ea53d86a44e48721ab0ee7841bf099e35ab4203
erikalasprilla <lasprillaerika@gmail.com>
2025-03-18 13:40:40 +0100

    Creación de javascript para sacar la media de edad

--- prueba-js/src/media.js
+++ prueba-js/src/media.js
@@ -1,5 +1,12 @@
 export default (json) => {
     // Do your thing here!
-    return "";
+    const calMedia = (datos) => {
+    const edades = datos.map(({ edad }) => edad);
+    const sum = edades.reduce((acc, edad) => acc + edad, 0);
+    return (sum / edades.length).toFixed(2);
+  };
+
+  return `${calMedia(json)}`;
+
   };
   
