9144e9e050fb267b126bc9222b48f214c8fa123d
erikalasprilla <lasprillaerika@gmail.com>
2025-03-18 13:41:16 +0100

    Creación de javascript para comprobar si word1 y word2 son anagramas

--- prueba-js/src/anagrama.js
+++ prueba-js/src/anagrama.js
@@ -1,5 +1,20 @@
 export default (word1, word2) => {
     // Do your thing here!
-    return false;
-  };
+    const sonAnagramas = (word1, word2) => {
+
+        word1 = word1.toLowerCase().replace(/\s/g, '');
+        word2 = word2.toLowerCase().replace(/\s/g, '');
+
+        if (word1.length !== word2.length) {
+            return false;
+        }
+
+        const ordenarWord1 = word1.split('').sort().join('');
+        const ordenarWord2 = word2.split('').sort().join('');
+
+        return ordenarWord1 === ordenarWord2;
+    };
+
+    return sonAnagramas(word1, word2);
+};
   
